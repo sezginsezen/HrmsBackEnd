66296e2d30a58530e1009851055b4dd6a1c76151
sezginsezen <szgszn@yandex.com>
2021-05-22 17:06:52 +0300

    Commit

---
+++ src/main/java/hrms/hrms/business/abstracts/JobPositionService.java
@@ -0,0 +1,10 @@
+package hrms.hrms.business.abstracts;
+
+import java.util.List;
+
+import hrms.hrms.entities.concretes.JobPosition;
+
+public interface JobPositionService
+{
+    List<JobPosition> getAll();
+}
